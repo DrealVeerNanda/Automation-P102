abb4855865ede7bf0592a442a74c95a6aa571e86
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2021-06-29 14:53:18 -0700

    Update tts.py

--- tts.py
+++ tts.py
@@ -3,7 +3,7 @@ from playsound import playsound
 import gtts
 #modules(gtts used to create the mp3 file and playsound to play the mp3 file)
 
-texttospeech = gtts.gTTS("Automation") #what the robot will say
+texttospeech = gtts.gTTS("Automation") #what the text to speech bot will say
 
 texttospeech.save("automationproject.mp3") #saves the file for the text to speech
 
