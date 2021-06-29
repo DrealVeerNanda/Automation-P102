a89703f04007ef174812918624350b1bc771fd3b
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2021-06-29 14:52:53 -0700

    Add files via upload

---
+++ tts.py
@@ -0,0 +1,11 @@
+from playsound import playsound
+
+import gtts
+#modules(gtts used to create the mp3 file and playsound to play the mp3 file)
+
+texttospeech = gtts.gTTS("Automation") #what the robot will say
+
+texttospeech.save("automationproject.mp3") #saves the file for the text to speech
+
+
+playsound("automationproject.mp3") #plays automation sound
